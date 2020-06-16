a48e6b265f01c474d8c66bc7c199f6a18b22b153
Jonathan Sheng <js165@rice.edu>
2020-06-16 06:34:30 -0400

    add project

--- main/urls.py
+++ main/urls.py
@@ -20,6 +20,8 @@ app_name = 'main'
 #Main urls points to views function called homepage
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('register/', views.register, name = 'register')
+    path('register/', views.register, name = 'register'),
+    path('logout/', views.logout_request, name = 'logout'),
+    path('login/', views.login_request, name='login')
 ]
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,16 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from .security import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#m$3p8-^d5z(bb(yijvo03bvx91c#g38t9%3*+r2m3**a#*i3j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
